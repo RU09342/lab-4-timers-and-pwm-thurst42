33437238e8be74324be630f0ea075ba5530ec508
thurst42 <thurst42@students.rowan.edu>
2017-10-15 17:43:17 -0700

    FR6989/F5529 update

--- Hardware PWM/F5529/main.c
+++ Hardware PWM/F5529/main.c
@@ -3,12 +3,14 @@
 int main(void) {
 WDTCTL = WDTPW + WDTHOLD;
 PM5CTL0 &= ~LOCKLPM5;
-    P9DIR |= BIT7; //Set pin 1.2 to the output direction
-    P1DIR |= BIT0;
+    P1DIR |= BIT0; //Set pin 1.2 to the output direction
+    P1DIR |= BIT2;
+
+
 
     P1REN |= BIT1; //Enable resistor for pin 1.3.
     P1OUT |= BIT1;
-    P1SEL0 |= BIT0;
+    P1SEL0 |= BIT2;
     TA0CCR0 = 1000; //The timer counts up to this.
     TA0CCTL1 = OUTMOD_7;
     TA0CCR1 = 500; //The PWM period, which is 0 uS.
@@ -17,11 +19,11 @@ PM5CTL0 &= ~LOCKLPM5;
 while(1)
 {
     if ((P1IN & BIT1) != BIT1) { //Listen for button presses.
-        P9OUT |= BIT7;
+        P1OUT |= BIT0;
         __delay_cycles(200000);
         TA0CCR1 = TA0CCR1 + 100; //Increment the PWM period
     if (TA0CCR1 > 1000) { TA0CCR1 = 0; }
-    else {P9OUT &= ~BIT7;}
+    else {P1OUT &= ~BIT0;}
 
     }
 }
