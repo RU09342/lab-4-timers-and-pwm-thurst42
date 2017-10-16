c66e56f46dea46b7a6e1a3bcb94e9fd992a0ea20
thurst42 <thurst42@students.rowan.edu>
2017-10-15 17:15:09 -0700

    FR5994 update

--- Hardware PWM/FR5994/main.c
+++ Hardware PWM/FR5994/main.c
@@ -11,18 +11,18 @@ PM5CTL0 &= ~LOCKLPM5;
     P5REN |= BIT6; //Enable resistor for pin 1.3.
     P5OUT |= BIT6;
     P1SEL0 |= BIT0;
-    TA1CCR0 = 1000; //The timer counts up to this.
-    TA1CCTL1 = OUTMOD_7;
-    TA1CCR1 = 500; //The PWM period, which is 0 uS.
-    TA1CTL = TASSEL_2 + MC_1;
+    TA0CCR0 = 1000; //The timer counts up to this.
+    TA0CCTL1 = OUTMOD_7;
+    TA0CCR1 = 500; //The PWM period, which is 0 uS.
+    TA0CTL = TASSEL_2 + MC_1;
 
 while(1)
 {
     if ((P5IN & BIT6) != BIT6) { //Listen for button presses.
         P1OUT |= BIT1;
-        __delay_cycles(100000);
-        TA1CCR0 = TA1CCR0 + 100; //Increment the PWM period
-    if (TA1CCR0 > 1000) { TA1CCR0 = 0; }
+        __delay_cycles(200000);
+        TA0CCR1 = TA0CCR1 + 100; //Increment the PWM period
+    if (TA0CCR1 > 1000) { TA0CCR1 = 0; }
     else {P1OUT &= ~BIT1;}
 
     }
